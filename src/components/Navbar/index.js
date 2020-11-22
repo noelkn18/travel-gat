02b6ece3b3041a29fb0872292c47fab9751611d5
Your Name <you@example.com>
2020-11-22 04:24:02 -0800

    init

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react'
+import { FaBars, FaTimes } from "react-icons/fa"
+import { IconContext } from "react-icons/lib"
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavIcon,
+  NavLogo,
+} from "./NavbarElements"
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const [scroll, setScroll] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    const changeNav = () => {
+        if (window.scrollY >= 80) {
+          setScroll(true)
+        } else {
+          setScroll(false)
+        }
+      }
+    
+      useEffect(() => {
+        changeNav()
+        window.addEventListener("scroll", changeNav)
+      }, [])
+
+return (
+    <>
+     <IconContext.Provider value={{ color: "#141414" }}>
+      <Nav active={scroll} click={click}>
+        <NavbarContainer>
+         <NavLogo to="/">
+            <NavIcon />
+              ESCAPE
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+        </NavbarContainer>
+      </Nav>  
+    </IconContext.Provider>
+   </> 
+    )
+}
+
+
+
+
+
+
+export default Navbar
