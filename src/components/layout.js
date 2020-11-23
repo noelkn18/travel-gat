a6616453ec72a8b3bae72f70e2f7b03a0a0181cf
Kazi Nobi (Noel) <52262312+noelkn18@users.noreply.github.com>
2020-11-23 13:33:54 -0800

    Add files via upload

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import PropTypes from "prop-types"
 import "./layout.css"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
--- src/pages/images.js
+++ src/pages/images.js
@@ -1,11 +1,12 @@
 import React from "react"
+import Image from "../components/image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Images = () => (
   <Layout>
-    <SEO title="Page two"/>
-    <h1>Images</h1>
+    <SEO title="Images" />
+    <Image />
   </Layout>
 )
 
